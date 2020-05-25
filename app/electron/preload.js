2600178c83f6adc89492067bcc9c3c4627df7495
florian CALLEWAERT <florian.callewaert@protonmail.com>
2020-05-25 17:43:24 +0200

    add show folder code using html list

--- app/electron/preload.js
+++ app/electron/preload.js
@@ -2,26 +2,56 @@
 // It has the same sandbox as a Chrome extension.
 
 const fs = require('fs')
+const util = require('util')
 const homePath = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE
 
-const showFolder = (path) => {
-  const folder = fs.readdirSync(path)
-  console.log(folder)
-}
+const readdir = util.promisify(fs.readdir)
 
-const replaceText = (selector, text) => {
-  const element = document.getElementById(selector)
-  if (element) element.innerText = text
+// module list BEGIN //
+const createListElement = () => {
+  let list = document.createElement('ul')
+  list.classList.add('app-list')
+  return list
 }
+const createListItem = (text) => {
+  let item = document.createElement('li')
+  item.classList.add('app-list-item')
+  item.innerText = text
+  return item
+}
+const createListItems = (textes) => {
+  let items = textes.reduce((fragment, text) => {
+    let item = createListItem(text)
+    fragment.appendChild(item)
+    return fragment
+  }, document.createDocumentFragment())
+  return items
+}
+const createList = (textes) => {
+  let list = createListElement()
+  let items = createListItems(textes)
+  list.appendChild(items)
+  return list
+}
+// module list END //
 
-const replaceTuto = () => {
-  const tutoTypes = ['chrome', 'node', 'electron']
-  for (const type of tutoTypes) {
-    replaceText(`${type}-version`, process.versions[type])
+const renderFolder = (files) => {
+  if (!files) {
+    return
   }
+  const domParent = document.getElementById('content')
+  const list = createList(files)
+  domParent.appendChild(list)
+}
+
+const showFolder = (path) => {
+  readdir(path)
+    .then((files) => {
+      renderFolder(files)
+    })
+    .catch((error) => console.warn(error))
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-  replaceTuto()
   showFolder(homePath)
 })
